8b9fe61ad96051353811ef9106d6d5e950c4e17f
JoniUzan <joniuzan33@gmail.com>
2024-10-19 22:20:14 +0300

    sdsd

--- src/index.ts
+++ src/index.ts
@@ -18,14 +18,14 @@ const PORT = process.env.PORT || 3000;
 const app: Express = express();
 const server = http.createServer(app);
 
-// export const io = new Server(server, {
-//   cors: {
-//     origin: "*", // Adjust this for production to the correct origin
-//     methods: ["GET", "POST", "PATCH", "DELETE"],
-//   },
-// });
+export const io = new Server(server, {
+  cors: {
+    origin: "*", // Adjust this for production to the correct origin
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  },
+});
 
-// setupSocketIO(io); // Set up Socket.IO
+setupSocketIO(io); // Set up Socket.IO
 
 async function main() {
   // Connect to database
